ad50729faad52c75dc807ce64d75fe199910757e
huangkai <18778335525@163.com>
2018-12-28 16:22:59 +0800

    ts函数

---
+++ func.ts
@@ -0,0 +1,14 @@
+// this和箭头函数
+
+let desk = {
+    suits: ['hearts', 'spades', 'clubs', 'diamonds'],
+    cards: Array(52),
+    createCardPicker: function () {
+        return () => {
+            let pickedCard: number = Math.floor(Math.random() * 52);
+            let pickedSuit: number = Math.floor(pickedCard / 13);
+
+            return {suit: this.suits[pickedSuit], card: pickedCard % 13}
+        }
+    }
+}
